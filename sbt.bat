b0f1aee354425e5965f1bbe339b523c2fdf88a58
Ryan Knight <ryknight@starbucks.com>
2017-04-02 12:54:03 -0600

    fixing build sbt

--- sbt.bat
+++ sbt.bat
@@ -1,2 +1,2 @@
 set SCRIPT_DIR=%~dp0
-java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
+java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M -Dsbt.ivy.home=ivy2/ -Divy.home=ivy2/ -jar "%SCRIPT_DIR%sbt-launch.jar" %*
